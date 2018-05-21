585f7d0fa784b3d7cc9b417b5e874e32cc4ec12b
CodeColorist <ChiChou@users.noreply.github.com>
2018-05-21 19:47:38 +0800

    Update send.js

--- send.js
+++ send.js
@@ -1,18 +1,32 @@
-const xpc_connection_get_name = new NativeFunction(
-  Module.findExportByName(null, 'xpc_connection_get_name'), 'pointer', ['pointer'])
 
-function logger(tag) {
-  return {
-    onEnter: function(args) {
-      const conn = ptr(args[0])
-      const name = Memory.readUtf8String(xpc_connection_get_name(conn))
-      console.log(tag, name)
-      console.log(new ObjC.Object(args[1]))
+function hook(symbol) {
+  Interceptor.attach(Module.findExportByName(null, symbol), {
+    onEnter: function (args) {
+      const conn = new ObjC.Object(args[0]);
+      const msg = new ObjC.Object(args[1]);
+      const content = [symbol + ':', conn, msg];
+      if (symbol === 'xpc_connection_send_message_with_reply' && !args[3].isNull()) {
+        // async reply
+        const cb = new ObjC.Block(args[3]).implementation;
+        const block =  new ObjC.Block({
+          retType: 'void',
+          argTypes: ['object'],
+          implementation: function(reply) {
+            console.log('async reply:\n' + new ObjC.Object(reply));
+            return cb(reply);
+          }
+        });
+      }
+      console.log(content.join('\n'));
     },
-    onLeave: function() {}
-  }
+    onLeave(retVal) {
+      if (symbol === 'xpc_connection_send_message_with_reply_sync') {
+        console.log('send sync, reply:\n' + new ObjC.Object(retVal));
+      }
+    }
+  })
 }
 
-Interceptor.attach(Module.findExportByName(null, 'xpc_connection_send_message'), logger('ignore'))
-Interceptor.attach(Module.findExportByName(null, 'xpc_connection_send_message_with_reply'), logger('async'))
-Interceptor.attach(Module.findExportByName(null, 'xpc_connection_send_message_with_reply_sync'), logger('sync'))
+hook('xpc_connection_send_message');
+hook('xpc_connection_send_message_with_reply');
+hook('xpc_connection_send_message_with_reply_sync');
