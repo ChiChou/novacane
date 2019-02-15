f6f5778f5a924abaabd187f5b19a09ebcd206e92
CodeColorist <codecolorist@gmail.com>
2019-02-15 14:43:43 +0800

    add webinspectord update README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# XPCShark
+# novacane
+
+## XPCShark
 Toy to have fun with XPC
+
+## safari
+Globally enable WebInspect for WebViews on macOS (without SIP) and iOS (requires jailbreak)
---
+++ safari/enable-inspect.js
@@ -0,0 +1,26 @@
+const SecTaskCopyValueForEntitlement = Module.findExportByName(null, 'SecTaskCopyValueForEntitlement');
+const CFRelease = new NativeFunction(Module.findExportByName(null, 'CFRelease'), 'void', ['pointer']);
+const CFStringGetCStringPtr = new NativeFunction(Module.findExportByName(null, 'CFStringGetCStringPtr'),
+  'pointer', ['pointer', 'uint32']);
+const kCFStringEncodingUTF8 = 0x08000100;
+const expected = [
+  'com.apple.security.get-task-allow',
+  'com.apple.private.webinspector.allow-remote-inspection',
+  'com.apple.private.webinspector.allow-carrier-remote-inspection',
+  'com.apple.webinspector.allow'
+];
+Interceptor.attach(SecTaskCopyValueForEntitlement, {
+  onEnter: function(args) {
+    const p = CFStringGetCStringPtr(args[1], kCFStringEncodingUTF8);
+    const ent = Memory.readUtf8String(p);
+  if (expected.indexOf(ent) > -1)
+      this.shouldOverride = true
+  },
+  onLeave: function(retVal) {
+    if (!this.shouldOverride)
+      return
+    if (!retVal.isNull())
+      CFRelease(retVal);
+    retVal.replace(ObjC.classes.NSNumber.numberWithBool_(1));
+  }
+})
