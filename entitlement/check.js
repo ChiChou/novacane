db56e24ffc7c91fe7fdfc7bcbb911c841fca12d5
CodeColorist <codecolorist@gmail.com>
2019-08-26 17:48:31 +0800

    print debug description

--- entitlement/check.js
+++ entitlement/check.js
@@ -5,8 +5,15 @@ const CFStringGetCStringPtr = new NativeFunction(Module.findExportByName(null, '
 const kCFStringEncodingUTF8 = 0x08000100;
 Interceptor.attach(SecTaskCopyValueForEntitlement, {
   onEnter: function (args) {
+    console.log(Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\n') + '\n')
     const p = CFStringGetCStringPtr(args[1], kCFStringEncodingUTF8);
     const ent = Memory.readUtf8String(p);
-    console.log('check for entitlement:', ent)
+    const description = SecTaskCopyDebugDescription(args[0])
+    if (!description.isNull()) {
+      const pDesc = CFStringGetCStringPtr(description, kCFStringEncodingUTF8)
+      console.log('enable inspector for', Memory.readUtf8String(pDesc))
+      CFRelease(description)
+    }
+    console.log('check for entitlement:', ent, 'target:', pDesc)
   }
 })
--- safari/enable-inspect.js
+++ safari/enable-inspect.js
@@ -1,4 +1,5 @@
 const SecTaskCopyValueForEntitlement = Module.findExportByName(null, 'SecTaskCopyValueForEntitlement');
+const SecTaskCopyDebugDescription = new NativeFunction(DebugSymbol.getFunctionByName('SecTaskCopyDebugDescription'), 'pointer', ['pointer']);
 const CFRelease = new NativeFunction(Module.findExportByName(null, 'CFRelease'), 'void', ['pointer']);
 const CFStringGetCStringPtr = new NativeFunction(Module.findExportByName(null, 'CFStringGetCStringPtr'),
   'pointer', ['pointer', 'uint32']);
@@ -10,17 +11,27 @@ const expected = [
   'com.apple.webinspector.allow'
 ];
 Interceptor.attach(SecTaskCopyValueForEntitlement, {
-  onEnter: function(args) {
-    const p = CFStringGetCStringPtr(args[1], kCFStringEncodingUTF8);
-    const ent = Memory.readUtf8String(p);
-  if (expected.indexOf(ent) > -1)
+  onEnter: function (args) {
+    const p = CFStringGetCStringPtr(args[1], kCFStringEncodingUTF8)
+    const ent = Memory.readUtf8String(p)
+    if (expected.indexOf(ent) > -1) {
       this.shouldOverride = true
+
+      const description = SecTaskCopyDebugDescription(args[0])
+      if (!description.isNull()) {
+        const pDesc = CFStringGetCStringPtr(description, kCFStringEncodingUTF8)
+        console.log('enable inspector for', Memory.readUtf8String(pDesc))
+        CFRelease(description)
+      }
+    }
   },
-  onLeave: function(retVal) {
+  onLeave: function (retVal) {
     if (!this.shouldOverride)
       return
+
     if (!retVal.isNull())
-      CFRelease(retVal);
+      CFRelease(retVal)
+
     retVal.replace(ObjC.classes.NSNumber.numberWithBool_(1));
   }
 })
