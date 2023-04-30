4ff517942ea91f79cee7c55abfa8605068b255cc
cc <codecolorist@gmail.com>
2023-04-30 22:40:24 +0200

    do not use tcp proxy usr socket directly

--- shell2/shell.js
+++ shell2/shell.js
@@ -1,4 +1,3 @@
-const net = require('net');
 const frida = require('frida');
 const { Client } = require('ssh2');
 
@@ -6,109 +5,76 @@ const { Client } = require('ssh2');
 /**
  * 
  * @param {frida.Device} device 
- * @returns {Number}
+ * @returns {Promise<Number>}
  */
 async function scan(device) {
-    const canidates = [22, 44]
-    for (const port of canidates) {
-        let channel
-
-        try {
-            channel = await device.openChannel(`tcp:${port}`);
-        } catch(e) {
-            continue
-        }
-
-        const yes = await new Promise((resolve) => {
-            channel.once('data', data => resolve(data.readUInt32BE() === 0x5353482d)); // SSH-
-            setTimeout(() => resolve(false), 500);
-        })
-
-        if (yes) return port
-    }
-
-    throw Error('port not found')
-}
-
-/**
- * 
- * @param {frida.Device} device
- * @returns {Promise<net.Server>}
- */
-async function iproxy(device) {
-    const port = await scan(device);
-    return new Promise((resolve) => {
-        const server = net.createServer(async (socket) => {
-            const channel = await device.openChannel(`tcp:${port}`);
-            socket.pipe(channel);
-            channel.pipe(socket);
-            socket.on('close', () => {
-                channel.unpipe();
-                socket.unpipe();
-                socket.unref();
-                channel.destroy();
-            }).on('error', console.error.bind(console));
-        }).listen(0, () => {
-            resolve(server);
+  const canidates = [22, 44]
+  for (const port of canidates) {
+    const channel = await device.openChannel(`tcp:${port}`);
+    const yes = await new Promise((resolve) => {
+      channel
+        .once('data', data => {
+          resolve(data.readUInt32BE() === 0x5353482d); // SSH-
+          channel.destroy();
+        }).once('error', () => {
+          resolve(false);
         });
-    });
+    })
+    if (yes) return port
+  }
+  throw Error('port not found')
 }
-
 
 async function main() {
-    const device = await frida.getUsbDevice();
-    const proxy = await iproxy(device);
+  const device = await frida.getUsbDevice();
+  const port = await scan(device);
+  const channel = await device.openChannel(`tcp:${port}`);
 
-    const port = proxy.address().port
-    const host = '127.1'
-    const username = 'root'
-    const password = 'alpine'
+  const username = 'root'
+  const password = 'alpine'
 
-    const pipeStream = stream => {
-        const { stdin, stdout, stderr } = process;
-        const { isTTY } = stdout;
+  const pipeStream = stream => {
+    const { stdin, stdout, stderr } = process;
+    const { isTTY } = stdout;
 
-        if (isTTY && stdin.setRawMode) stdin.setRawMode(true);
+    if (isTTY && stdin.setRawMode) stdin.setRawMode(true);
 
-        stream.pipe(stdout);
-        stream.stderr.pipe(stderr);
-        stdin.pipe(stream);
+    stream.pipe(stdout);
+    stream.stderr.pipe(stderr);
+    stdin.pipe(stream);
 
-        const onResize = isTTY && (() => stream.setWindow(stdout.rows, stdout.columns, null, null));
-        if (isTTY) {
-            stream.once('data', onResize)
-            process.stdout.on('resize', onResize)
-        }
-        stream.on('close', () => {
-            if (isTTY) process.stdout.removeListener('resize', onResize)
-            stream.unpipe()
-            stream.stderr.unpipe()
-            stdin.unpipe()
-            if (stdin.setRawMode) stdin.setRawMode(false)
-            stdin.unref()
-        })
+    const onResize = isTTY && (() => stream.setWindow(stdout.rows, stdout.columns, null, null));
+    if (isTTY) {
+      stream.once('data', onResize)
+      process.stdout.on('resize', onResize)
     }
+    stream.on('close', () => {
+      if (isTTY) process.stdout.removeListener('resize', onResize)
+      stream.unpipe()
+      stream.stderr.unpipe()
+      stdin.unpipe()
+      if (stdin.setRawMode) stdin.setRawMode(false)
+      stdin.unref()
+    })
+  }
 
-    const conn = new Client();
-    await new Promise((resolve) => {
-        conn.on('ready', function () {
-            conn.shell({ term: process.env.TERM || 'vt100' }, (err, stream) => {
-                if (err) {
-                    reject(err)
-                    return
-                }
-                pipeStream(stream)
-                stream.on('close', () => resolve(true))
-            });
-        }).connect({
-            host,
-            port,
-            username,
-            password
-        });
+  const conn = new Client();
+  conn.on('ready', function () {
+    conn.shell({ term: process.env.TERM || 'vt100' }, (err, stream) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      pipeStream(stream)
+      stream.on('close', () => {
+        conn.end();
+      })
     });
-
-    process.exit();
+  }).connect({
+    sock: channel,
+    username,
+    password
+  });
 }
 
 main()
