28bd0a37ce80fed172af5ba41b7807300d0e2b1b
cc <codecolorist@gmail.com>
2023-04-30 21:44:17 +0200

    faster implementation

--- xpcshark/incoming.js
+++ xpcshark/incoming.js
@@ -1,7 +1,16 @@
-Interceptor.attach(DebugSymbol.getFunctionByName('_xpc_connection_call_event_handler'), {
-  onEnter: function(args) {
-    console.log('call event handler:')
-    console.log(new ObjC.Object(args[0]));
-    console.log(new ObjC.Object(args[1]));
-  }
-});
+Module.enumerateSymbols('libxpc.dylib', {
+  onMatch(symbol) {
+    if (symbol.name == '_xpc_connection_call_event_handler') {
+      Interceptor.attach(symbol.address, {
+        onEnter(args) {
+          console.log('call event handler:')
+          console.log(new ObjC.Object(args[0]));
+          console.log(new ObjC.Object(args[1]));
+        }
+      })
+
+      return 'stop';
+    }
+  },
+  onComplete() {}
+})
