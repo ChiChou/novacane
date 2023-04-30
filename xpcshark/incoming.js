5a0730ab9b1a62c8161f8752d9ca666e4d915291
cc <codecolorist@gmail.com>
2023-05-01 00:19:03 +0200

    use promise

--- xpcshark/incoming.js
+++ xpcshark/incoming.js
@@ -1,16 +1,31 @@
-Module.enumerateSymbols('libxpc.dylib', {
-  onMatch(symbol) {
-    if (symbol.name == '_xpc_connection_call_event_handler') {
-      Interceptor.attach(symbol.address, {
-        onEnter(args) {
-          console.log('call event handler:')
-          console.log(new ObjC.Object(args[0]));
-          console.log(new ObjC.Object(args[1]));
+function findHandler() {
+  return new Promise((resolve, reject) => {
+    let found = false;
+    Module.enumerateSymbols('libxpc.dylib', {
+      onMatch(symbol) {
+        if (symbol.name == '_xpc_connection_call_event_handler') {
+          resolve(symbol.address);
+          found = true;
+          return 'stop';
         }
-      })
+      },
+      onError: reject,
+      onComplete() {
+        if (!found) reject(new Error('not found'));
+      }
+    })
+  })
+}
 
-      return 'stop';
+async function main() {
+  const handler = await findHandler();
+  Interceptor.attach(handler, {
+    onEnter(args) {
+      console.log('call event handler:');
+      console.log(new ObjC.Object(args[0]));
+      console.log(new ObjC.Object(args[1]));
     }
-  },
-  onComplete() {}
-})
+  })
+}
+
+main()
